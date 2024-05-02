660614418cf1c496069550b4ec93858be6fc4cd2
kwonyul <kw990812@gmail.com>
2024-05-02 10:47:51 +0900

    회원목록 확인 기능 추가

--- src/main/java/com/prj1/controller/MemberController.java
+++ src/main/java/com/prj1/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.prj1.domain.Member;
 import com.prj1.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,12 @@ public class MemberController {
     public String signup(Member member) {
         service.signup(member);
 
-        return null;
+        return "redirect:/";
+    }
+
+    @GetMapping("list")
+    public String list(Model model) {
+        model.addAttribute("memberList", service.list());
+        return "member/list";
     }
 }
--- src/main/java/com/prj1/mapper/MemberMapper.java
+++ src/main/java/com/prj1/mapper/MemberMapper.java
@@ -3,6 +3,9 @@ package com.prj1.mapper;
 import com.prj1.domain.Member;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface MemberMapper {
@@ -13,4 +16,12 @@ public interface MemberMapper {
             VALUES (#{email}, #{password}, #{nickName})
             """)
     int insert(Member member);
+
+
+    @Select("""
+            SELECT *
+            FROM member
+            ORDER BY id DESC
+            """)
+    List<Member> selectAll();
 }
